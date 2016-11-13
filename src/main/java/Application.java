6fb9fda767b13f5cc8d6c5cbaa3be74c6e81e1d1
Valeriy <vvalmail@gmail.com>
2016-11-13 22:41:28 +0200

    Added Spring Boot

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by val on 13.11.16.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
